dbfc2de48ae6f5180fa0c028ea925a50913a8ef9
bocastle <bocastle1213@gmail.com>
2025-01-18 14:33:07 +0900

    더미데이터 추가

--- src/components/CustomCalendar/types.ts
+++ src/components/CustomCalendar/types.ts
@@ -2,3 +2,7 @@ export interface CalendarDay {
   day: number; // 날짜 숫자
   isCurrentMonth: boolean; // 현재 달 여부
 }
+export interface Schedule {
+  time: string; //시간
+  description: string; //내용
+}
---
+++ src/dummyData/dummySchedules.ts
@@ -0,0 +1,7 @@
+import { Schedule } from "@/components/CustomCalendar/types";
+
+export const dummySchedules: Schedule[] = [
+  { time: "09:00", description: "팀 미팅" },
+  { time: "11:30", description: "오전 회의" },
+  { time: "14:30", description: "점심 회의" },
+];
