dfc9e1744732b42b30fca9f70aead9162493733d
bocastle <bocastle1213@gmail.com>
2025-01-19 15:04:04 +0900

    배포 준비 및 env 추가

--- next.config.mjs
+++ next.config.mjs
@@ -2,5 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
 };
+if (process.env.NEXT_PUBLIC_NODE_ENV === "prod") {
+  nextConfig.compiler = {
+    removeConsole: {
+      exclude: ["error", "warn"],
+    },
+  };
+}
 
 export default nextConfig;
